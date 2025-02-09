0797f86e6d625df28dfafd20e38a1819344de3a5
Nelson Zarate <nelsonpinto1415@gmail.com>
2025-02-09 01:16:33 +0000

    add: session 6 exercice 2

--- session_6/ex2.js
+++ session_6/ex2.js
@@ -1,17 +1,20 @@
-function traverse(obj, path = []) {  
-    if (typeof obj !== 'object') {  
-      console.log(path.join('.'), ':', obj);  
+function traverse(obj, path = [], newObj = {}) {  
+  if (typeof obj !== 'object' || obj === null) {
+      newObj[path.join('.')] = obj;
       return;  
     }  
     for (const key in obj) {  
-      traverse(obj[key], [...path, key]);  
-    }  
-  }  
+      traverse(obj[key], [...path, key], newObj);  
+  }
+
   
+    return newObj;
+  }  
   const nested = {  
     a: 1,  
     b: { c: 2, d: { e: 3 } },  
     f: [4, 5]  
   };  
 
-traverse(nested);
+const newObj = traverse(nested);
+console.log(newObj);
