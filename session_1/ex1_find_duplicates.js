8debad139d948d22c7e0f71c04e7a5a6c91b4b52
Nelson Zarate <nelsonpinto1415@gmail.com>
2025-02-11 15:36:07 +0000

    add:session 1 exercices

---
+++ session_1/ex1_find_duplicates.js
@@ -0,0 +1,11 @@
+function findDuplicates(array_duplicado){
+    let array_ordenado = [];
+    for(i = 0 ; i < array_duplicado.length; i++){
+        if(array_duplicado[i] !== array_duplicado[i+1]){
+            array_ordenado.push(array_duplicado[i])
+        }
+    }
+    return array_ordenado;
+}
+
+console.log(findDuplicates(array=[1,2,3,4,4,4,4,6,7,7]));
