6fba6a9dadc91f430c577ed7b212b986b4a8cb09
Nelson Zarate <nelsonpinto1415@gmail.com>
2025-02-24 02:01:42 +0000

    chore password gen password list

--- session_13/password_Gen/pwgen.js
+++ session_13/password_Gen/pwgen.js
@@ -1,17 +1,44 @@
+import { writeFileSync, readFileSync, existsSync} from "fs";
 export function passwordGen(argsMap) {
-    // Set defaults
     const options = {
-        length: 12,
+        length: 2,
         uppercase: false,
         numbers: false,
         ...argsMap 
     };
-
     options.length = parseInt(options.length) || 12;
 
-    if (options.length < 12 || options.length > 128) {
-        throw new Error('Password length must be between 12 and 128 characters');
+    if (options.length < 2 || options.length > 100) {
+        throw new Error('Password length must be between 12 and 100 characters');
     }
+
+    let passwordList = [];
+    if (existsSync('pw-config.json')) {
+        try {
+            const fileContent = readFileSync('pw-config.json', 'utf-8');
+            passwordList= JSON.parse(fileContent);
+            if (!Array.isArray(passwordList)) {
+                passwordList = [];
+            }
+        } catch (error) {
+            passwordList = [];
+        }
+    }
+    
+
+    let password = generatePassword(options);
+
+    while(passwordList.includes(password)){
+        password = generatePassword(options);
+    }
+
+    passwordList.push(password)
+    writeFileSync('pw-config.json', JSON.stringify(passwordList));
+    return password;
+
+}
+
+function generatePassword(options){
 
     const lowercase = 'abcdefghijklmnopqrstuvwxyz';
     const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -22,10 +49,10 @@ export function passwordGen(argsMap) {
     if (options.numbers) pool += numbers;
 
     let password = '';
+
     for (let i = 0; i < options.length; i++) {
         const randomIndex = Math.floor(Math.random() * pool.length);
         password += pool[randomIndex];
     }
-
     return password;
 }
